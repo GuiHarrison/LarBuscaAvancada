786e748401060e05e30674a8651781909c1ebb1b
GuiHarrison <guilherme@ingagedigital.com.br>
2017-07-04 18:28:46 -0300

    Responsivo

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,14 @@
-Webflow.require('ix').init([
-	{"slug":"seta-2","name":"seta 2","value":{"style":{},"triggers":[{"type":"dropdown","selector":".seta-2","descend":true,"preserve3d":true,"stepsA":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"180deg"}],"stepsB":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"0deg"}]}]}},
-	{"slug":"seta3","name":"seta3","value":{"style":{},"triggers":[{"type":"dropdown","selector":".seta-3","descend":true,"preserve3d":true,"stepsA":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"180deg"}],"stepsB":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"0deg"}]}]}},
-	{"slug":"seta-4","name":"seta 4","value":{"style":{},"triggers":[{"type":"dropdown","selector":".seta-4","descend":true,"preserve3d":true,"stepsA":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"180deg"}],"stepsB":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"0deg"}]}]}},
-	{"slug":"seta-1","name":"seta 1","value":{"style":{},"triggers":[{"type":"dropdown","selector":".seta-1","descend":true,"preserve3d":true,"stepsA":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"180deg"}],"stepsB":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"0deg"}]}]}},
-	{"slug":"seta-5","name":"seta 5","value":{"style":{},"triggers":[{"type":"dropdown","selector":".seta-5","descend":true,"preserve3d":true,"stepsA":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"180deg"}],"stepsB":[{"transition":"transform 300ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"0deg"}]}]}},
+var mq = window.matchMedia( "(min-width: 991px)" );
 
-	{"slug":"busca-avancada","name":"busca avancada","value":{"style":{},"triggers":[{"type":"click","selector":".pai-busca-avancada","stepsA":[{"height":"250px","transition":"transform 200 ease 0, height 350ms ease 0"}],"stepsB":[{"height":"0px","transition":"height 350ms ease 0"}]},{"type":"click","selector":".icon","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"180deg"}],"stepsB":[{"transition":"transform 350ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"0deg"}]},{"type":"click","selector":".h1-home","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]},{"type":"click","selector":".h2-home","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]},{"type":"click","selector":".div-buscador","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]}]}}
-]);
+if (mq.matches) {
+	Webflow.require('ix').init([
+		{"slug":"busca-avancada","name":"busca avancada","value":{"style":{},"triggers":[{"type":"click","selector":".pai-busca-avancada","stepsA":[{"height":"250px","transition":"transform 200 ease 0, height 350ms ease 0"}],"stepsB":[{"height":"0px","transition":"height 350ms ease 0"}]},{"type":"click","selector":".icon","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"180deg"}],"stepsB":[{"transition":"transform 350ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"0deg"}]},{"type":"click","selector":".h1-home","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]},{"type":"click","selector":".h2-home","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]},{"type":"click","selector":".div-buscador","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]}]}}
+	]);
+} else {
+	Webflow.require('ix').init([
+		{"slug":"busca-avancada","name":"busca avancada","value":{"style":{},"triggers":[{"type":"click","selector":".pai-busca-avancada","stepsA":[{"height":"370px","transition":"transform 200 ease 0, height 350ms ease 0"}],"stepsB":[{"height":"0px","transition":"height 350ms ease 0"}]},{"type":"click","selector":".icon","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"180deg"}],"stepsB":[{"transition":"transform 350ms ease 0","rotateX":"0deg","rotateY":"0deg","rotateZ":"0deg"}]},{"type":"click","selector":".h1-home","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]},{"type":"click","selector":".h2-home","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]},{"type":"click","selector":".div-buscador","preserve3d":true,"stepsA":[{"transition":"transform 350ms ease 0","x":"0px","y":"-80px","z":"0px"}],"stepsB":[{"transition":"transform 350ms ease 0","x":"0px","y":"0px","z":"0px"}]}]}}
+	]);
+}
 
 $(function() {
 	'use strict';
@@ -17,7 +19,21 @@ $(function() {
 		$precoMin1 = $('#field-3'),
 		$precoMax1 = $('#field-4'),
 		$precoMin2 = $('#field-5'),
-		$precoMax2 = $('#field-6');
+		$precoMax2 = $('#field-6'),
+		$botaoComAl = $('.botao-comprar');
+
+	$botaoComAl.click(function(e) {
+		e.preventDefault();
+		// $(this).toggleClass('ativo');
+
+		if ($(this).hasClass('ativo')) {
+			$botaoComAl.removeClass('ativo');
+		} else { 
+			$botaoComAl.removeClass('ativo');
+			$(this).addClass('ativo');
+		}
+
+	});
 	
 	$baFiltro.on('click', function() {
 		var $thisList = $(this).find('.droplist'),
@@ -39,6 +55,7 @@ $(function() {
 		}
 	});
 
+	//Criar slide
 	noUiSlider.create($barra1, {
 		start: [100000, 1000000],
 		connect: [false, true, false],
